a316f3c734b576b48514014b0b6a1678237e07ad
wuke1993 <2297184250@qq.com>
2017-04-24 14:40:44 +0800

    Rename as UserProfilesController.

--- src/org/silkroad/controller/UserProfilesController.java
+++ src/org/silkroad/controller/UserProfilesController.java
@@ -20,7 +20,7 @@ import com.mongodb.client.MongoCollection;
 * Description : 
 */
 @WebServlet("/UserProfiles")
-public class UserProfiles extends HttpServlet {
+public class UserProfilesController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
