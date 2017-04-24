a93a25d65f9a99bf9561fbea0dfdefe08db2336e
wuke1993 <2297184250@qq.com>
2017-04-24 14:41:19 +0800

    Rename as UserViewedHistoryController.

--- src/org/silkroad/controller/UserViewedHistoryController.java
+++ src/org/silkroad/controller/UserViewedHistoryController.java
@@ -3,6 +3,11 @@ package org.silkroad.controller;
 import static com.mongodb.client.model.Filters.eq;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,45 +16,51 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.bson.Document;
 import org.silkroad.utility.MongoConn;
+import org.silkroad.utility.MySQLConn;
 
 import com.mongodb.client.MongoCollection;
 
 /**
-* @author : wuke
-* @date   : 2017年4月23日下午3:30:06
-* Title   : UserViewedHistory
-* Description : 
-*/
+ * @author : wuke
+ * @date : 2017年4月23日下午3:30:06 Title : UserViewedHistory Description :
+ */
 @WebServlet("/UserViewedHistory")
-public class UserViewedHistory extends HttpServlet {
+public class UserViewedHistoryController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doPost(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// Get userId and res_type
-		String user_id = request.getParameter("user_id");
+		// String user_id = request.getParameter("user_id");
+		String user_id = request.getSession().getAttribute("user_id").toString(); // 
 		String res_type = request.getParameter("res_type");
 		
+		user_id = "888";
+
 		// Query from MongoDB
 		String collectionName = "user_viewed_" + res_type;
 		MongoCollection<Document> collection = MongoConn.getMongoCollection("silkRoad", collectionName);
-		
+
 		Document doc = collection.find(eq("user_id", user_id)).first();
-		
+
 		String json = doc.toJson();
-		
+
 		// Respond to the request
 		response.setContentType("application/json;charset=utf-8");
-		response.addHeader("Access-Control-Allow-Origin", "*");
-		response.getWriter().write(json);    
+		response.addHeader("Access-Control-Allow-Origin", "*"); // 控制访问
+		response.getWriter().write(json);
 	}
 }
