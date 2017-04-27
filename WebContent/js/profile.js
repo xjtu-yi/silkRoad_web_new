6e261ce9260a6b9e53deacfa52affcec80625938
wuke1993 <2297184250@qq.com>
2017-04-27 22:05:02 +0800

    Web page about user profiles.

--- WebContent/js/profile.js
+++ WebContent/js/profile.js
@@ -2,146 +2,86 @@
  * http://202.117.15.155:8080/silkRoad/profiles.html
  */
 
-function toPercent(point){
-    var str=Number(point*100).toFixed(2);
-    str+="%";
-    return str;
-}
-
 $(document).ready(function () {
-    var percents = [];
+    var time = [];
     var times = [];
     var total = 0;
     
     $.ajax({
-        url:"http://202.117.15.155:8080/silkRoad/userInfo?user_id=103",
-        // url:"http://123.206.87.123:8080/silkRoad2/user_info",
+        url:"http://202.117.15.155:8080/silkRoad/userInfo?user_id=802",
         async:false,
         success:function (jsonData) {
-            for (var i = 0; i < jsonData.length; i++) {
-                var tem = jsonData[i];
-                
-                total = tem.company + tem.country + tem.pebook + tem.regulation + tem.uansr + tem.uebook;
-                
-                percents = [tem.company/total, tem.country/total, tem.pebook/total, tem.regulation/total, 
-                    tem.uansr/total, tem.uebook/total];
-                
-                times=[{name:"company",value:tem.company}, {name:"country",value:tem.country},
-                    {name:"pebook",value:tem.pebook}, {name:"regulation",value:tem.regulation},
-                    {name:"uansr",value:tem.uansr}, {name:"uebook",value:tem.uebook}];
-            }
+            var tem = jsonData;
+            
+            time = [tem.company, tem.country, tem.pebook, tem.regulation, tem.uansr, tem.uebook];
+            // alert(time);
+            
+            times=[{name:"Company",value:tem.company}, {name:"Country",value:tem.country},
+                {name:"Pebook",value:tem.pebook}, {name:"History and Culture",value:tem.uebook},
+                {name:"Education of Science",value:tem.uansr}, {name:"IEEE",value:tem.regulation}];
         }
     });
     
-    alert(percents[0]);
-    document.getElementById('company').innerHTML = toPercent(percents[0]);
-    document.getElementById('country').innerHTML = toPercent(percents[1]);
-    document.getElementById('pebook').innerHTML = toPercent(percents[2]);
-    document.getElementById('regulation').innerHTML = toPercent(percents[3]);
-    document.getElementById('uansr').innerHTML = toPercent(percents[4]);
-    document.getElementById('uebook').innerHTML = toPercent(percents[5]);
+    // alert(percents);
+    document.getElementById('company').innerHTML = time[0];
+    document.getElementById('country').innerHTML = time[1];
+    document.getElementById('pebook').innerHTML = time[2];
+    document.getElementById('regulation').innerHTML = time[3];
+    document.getElementById('uansr').innerHTML = time[4];
+    document.getElementById('uebook').innerHTML = time[5];
 
-    /*require.config({
-        paths: {
-            echarts: './build/dist/'
-        }
-    });
-    require(
-        [
-            'echarts',
-            'echarts/chart/bar', // 使用柱状图就加载bar模块，按需加载
-            'echarts/chart/pie'
-        ],
-        function (ec) {
-            // 基于准备好的dom，初始化echarts图表
-            var myChart = ec.init(document.getElementById('bar'));
-            option = {
-                title: {
-                    text: 'Number of visiting the type of sources',
-                    // subtext: '纯属虚构',
-                    x: 'right'
-                },
-                tooltip: {
-                    trigger: 'item',
-                    formatter: "{a} <br/>{b} : {c} ({d}%)"
-                },
-                legend: {
-                    orient: 'vertical',
-                    x: 'left',
-                    data: ['News', 'Paper', 'Ebook', 'Patent', 'Conference']
-                },
-                calculable: true,
-                series: [
-                    {
-                        name: 'type of source',
-                        type: 'pie',
-                        radius: '55%',
-                        center: ['50%', "60%"],
-                        data:data
-                    }
-                ]
-            };
+    // ECharts
+    var myChart = echarts.init(document.getElementById('bar')); // 基于准备好的dom，初始化echarts实例
+    
+    var option = { // 指定图表的配置项和数据
+	    title : {
+	        text: 'Visiting times',
+	        // subtext: '纯属虚构',
+	        x:'center'
+	    },
+	    tooltip : {
+	        trigger: 'item',
+	        formatter: "{a} <br/>{b} : {c} ({d}%)"
+	    },
+	    legend: {
+	        orient : 'vertical',
+	        x : 'left',
+	        data:['Company','Country','Pebook','History and Culture','Education of Science', 'IEEE']
+	    },
+	    toolbox: {
+	        show : true,
+	        feature : {
+	            mark : {show: true},
+	            dataView : {show: true, readOnly: false},
+	            magicType : {
+	                show: true, 
+	                type: ['pie', 'funnel'],
+	                option: {
+	                    funnel: {
+	                        x: '25%',
+	                        width: '50%',
+	                        funnelAlign: 'left',
+	                        max: 1548
+	                    }
+	                }
+	            },
+	            restore : {show: true},
+	            saveAsImage : {show: true}
+	        }
+	    },
+	    calculable : true,
+	    series : [
+	        {
+	            name:'Visiting times',
+	            type:'pie',
+	            radius : '40%',
+	            center: ['50%', '60%'],
+	            data:times
+	        }
+	    ]
+    };
 
-            var dataAxis = ['Company', 'Country', 'Pebook', 'IEEE', 'History and Culture'];
-            var data4 = [];
-            for (var i = 0;i<data.length;i++){
-                data4[i]=data[i].value;
-            }
-            var yMax = 500;
-            var dataShadow = [];
-
-            for (var i = 0; i < data4.length; i++) {
-                dataShadow.push(yMax);
-            }
-
-            option2 = {
-                title : {
-                    text: 'Number of visiting the type of sources',
-                    x:'center'
-                },
-                tooltip : {
-                    trigger: 'item'
-                },
-
-                calculable : true,
-                xAxis : [
-                    {   name : 'category',
-                        type : 'category',
-                        data:dataAxis
-                    }
-                ],
-                yAxis : [
-                    {   name:'number',
-                        type : 'value'
-                    }
-                ],
-                series : [
-
-                    {
-                        name:"number",
-                        type:"bar",
-                        itemStyle: {
-                            normal: {
-                                label : {
-                                    show: true, position: 'top'
-                                },
-                                color: '#6495ED'
-                            }
-                        },
-                        data:data4
-                    }
-
-                ]
-            };
-
-            myChart2.setOption(option2);
-
-            setTimeout(function () {
-                window.onresize = function () {
-                    myChart2.resize();
-                }
-            }, 200)
-
-        }
-    );*/
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(option);
+    
 });
