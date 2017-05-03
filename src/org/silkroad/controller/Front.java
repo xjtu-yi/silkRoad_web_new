cba42acb0c375fe5bc0ca43a956bb68d65e1a0d0
wuke1993 <2297184250@qq.com>
2017-05-03 19:30:36 +0800

    Servlet for

---
+++ src/org/silkroad/controller/Front.java
@@ -0,0 +1,64 @@
+package org.silkroad.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Front
+ */
+@WebServlet("/Front")
+public class Front extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Front() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String type = request.getParameter("type");
+        
+        if(type.equals("4")) // 
+        	response.sendRedirect("http://personalize.ikcest.xjtu.io:8080/silkAdmin/index.html");
+        else {
+			String user_id = null;
+			switch(type) {
+			case "1":
+				user_id = "453"; // student
+				break;
+			case "2":
+				user_id = "101"; // engineer
+				break;
+			case "3":
+				user_id = "104"; // scholar
+				break;
+			default:
+				user_id = "453";
+			}
+			
+			HttpSession session = request.getSession();
+			session.setAttribute("user_id", user_id);
+			session.setAttribute("user_type", type);
+		    
+		    request.getRequestDispatcher("profiles.html").forward(request, response);
+	    }
+	}
+}
