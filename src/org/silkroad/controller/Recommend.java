6cad7f053a0f014828c878fec02fe9bd74579f40
wuke1993 <2297184250@qq.com>
2017-05-08 22:19:27 +0800

    Offline processing the recommendations.

--- src/org/silkroad/controller/Recommend.java
+++ src/org/silkroad/controller/Recommend.java
@@ -8,46 +8,54 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.silkroad.recommend.GetRec;
+import org.bson.Document;
+import org.silkroad.utility.MongoConn;
+
+import com.mongodb.client.MongoCollection;
 
 /**
  * Servlet implementation class Recommend
  */
-@WebServlet("/Recommend0")
+@WebServlet("/Recommend")
 public class Recommend extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Recommend() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public Recommend() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String user_id = (String) request.getSession().getAttribute("user_id");
 		int type = Integer.parseInt(request.getParameter("type"));
+		MongoCollection<Document> collection = MongoConn.getMongoCollection("silkRoad", "recommendInfo");
+		Document document = new Document();
+		document.put("user_id", user_id);
+		document.put("res_type_id", type);
+		Document d = collection.find(document).first();
+		String json = d.get("data").toString();
 		
-		String json=null;
-		try {
-			json = GetRec.getRec(type, user_id);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
+		System.out.println(json);
+
 		response.setContentType("application/json;charset=utf-8");
 		response.addHeader("Access-Control-Allow-Origin", "*");
 		response.getWriter().write(json);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
